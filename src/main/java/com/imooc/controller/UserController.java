48745e4220231bfdc0f29cb5b733ddd7d644c4ad
qy-gitlab <439831370@qq.com>
2018-07-26 23:29:33 +0800

    第一次提交

---
+++ src/main/java/com/imooc/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class UserController {
+    @RequestMapping("getUser")
+    public User getUser(){
+        User u = new User();
+        u.setName("jack");
+        u.setAge(23);
+        u.setPassword("123456");
+        u.setBirthday(new Date());
+        return u;
+
+    }
+}
