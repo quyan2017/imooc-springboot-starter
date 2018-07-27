f62e0e3252ed84e1bf6355e559471dbf3e6b5028
qy-gitlab <439831370@qq.com>
2018-07-27 13:08:06 +0800

    tomcat、session的一些配置

--- src/main/java/com/imooc/controller/UserController.java
+++ src/main/java/com/imooc/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.IMoocJSONResult;
 import com.imooc.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,13 @@ import java.util.Date;
 @RestController
 public class UserController {
     @RequestMapping("getUser")
-    public User getUser(){
+    public IMoocJSONResult getUser(){
         User u = new User();
         u.setName("jack");
         u.setAge(23);
         u.setPassword("123456");
         u.setBirthday(new Date());
-        return u;
+        return IMoocJSONResult.ok(u);
 
     }
 }
